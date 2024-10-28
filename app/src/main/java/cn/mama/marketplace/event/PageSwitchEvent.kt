f9058a45dd1572bd6bb58fbb0f32f8ca7de8cde5
cocomanbar <125322078@qq.com>
2024-10-28 10:22:05 +0800

    登录页的转场动画
    bottom to top

---
+++ app/src/main/java/cn/mama/marketplace/event/PageSwitchEvent.kt
@@ -0,0 +1,4 @@
+package cn.mama.marketplace.event
+
+/** 通知Tab切换页面，从[activityClass]页面发出来 */
+class PageSwitchEvent(private val activityClass: Class<*>? = null) : MessageEvent()
--- app/src/main/java/cn/mama/marketplace/event/RefreshEvent.kt
+++ app/src/main/java/cn/mama/marketplace/event/RefreshEvent.kt
@@ -1,4 +1,4 @@
 package cn.mama.marketplace.event
 
 /** 通知 [activityClass] 刷新界面消息 */
-class PageRefreshEvent(val activityClass: Class<*>? = null) : MessageEvent()
+class PageRefreshEvent(private val activityClass: Class<*>? = null) : MessageEvent()
